f12e87280ec874f71c88819c1d3ae81c8865bf87
clouddevops7vg-bit <clouddevops7vg@gmail.com>
2026-05-20 14:18:19 -0500

    Refactor main.tf to comment out EKS and S3 module configurations for future implementation

--- main.tf
+++ main.tf
@@ -26,39 +26,39 @@ module "vpc" {
   tags = local.tags
 }
 
-################################################################################
-# EKS Module
-################################################################################
+# ################################################################################
+# # EKS Module
+# ################################################################################
+# 
+# module "eks" {
+#   source = "./modules/eks"
+# 
+#   cluster_name       = local.name
+#   kubernetes_version = local.kubernetes_version
+#   private_subnets   = module.vpc.private_subnets
+#   public_subnets    = module.vpc.public_subnets
+# 
+#   instance_types = ["t3.small"]
+#   capacity_type  = "SPOT"
+#   desired_size   = 1
+#   max_size       = 2
+#   min_size       = 1
+# 
+#   tags = local.tags
+# 
+#   depends_on = [module.vpc]
+# }
 
-module "eks" {
-  source = "./modules/eks"
-
-  cluster_name       = local.name
-  kubernetes_version = local.kubernetes_version
-  private_subnets   = module.vpc.private_subnets
-  public_subnets    = module.vpc.public_subnets
-
-  instance_types = ["t3.small"]
-  capacity_type  = "SPOT"
-  desired_size   = 1
-  max_size       = 2
-  min_size       = 1
-
-  tags = local.tags
-
-  depends_on = [module.vpc]
-}
-
-################################################################################
-# S3 Module
-################################################################################
-
-module "s3" {
-  source = "./modules/s3"
-
-  bucket_name       = "my-terraform-bucket-${data.aws_caller_identity.current.account_id}"
-  enable_versioning = true
-  sse_algorithm     = "AES256"
-
-  tags = local.tags
-}
+# ################################################################################
+# # S3 Module
+# ################################################################################
+# 
+# module "s3" {
+#   source = "./modules/s3"
+# 
+#   bucket_name       = "my-terraform-bucket-${data.aws_caller_identity.current.account_id}"
+#   enable_versioning = true
+#   sse_algorithm     = "AES256"
+# 
+#   tags = local.tags
+# }
